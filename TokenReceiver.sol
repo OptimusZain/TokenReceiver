9675237629d72ef82314caa02c488209317b7b05
Ali Wisam Ch <aliwisaam73@gmail.com>
2021-11-24 13:12:29 +0500

    Update TokenReceiver.sol

--- TokenReceiver.sol
+++ TokenReceiver.sol
@@ -1,31 +1,37 @@
-pragma solidity ^0.6.12;
+pragma solidity ^0.8.7;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TokenReceiver is Ownable{
     
-    event Received(address, uint);
+    event EtherReceived(address from, uint value);
+    event vaultChanged(address new_address);
     event TokenReceived(string _name, uint _decimals, uint _amount, address _from);
     
+
+    //public list show struct[{bool,name, symbol, address}]
+    
+    uint counter; 
+    //7
+    //f() return{7}
+
     ERC20 ERC20Contract;
     mapping(address => bool) public whitelist;
     
     address payable vault;
     
-    constructor(address payable _vault) Ownable() public {
-        vault = _vault;
+    constructor() {
+        vault = payable(owner());
     } 
     
-    function receiveTokens(address [] memory _tokenAddr, uint [] memory _amount) external {
-        require(checkWhitelisted(_tokenAddr), "Token not accepted");
-        
-        for(uint i = 0; i < _tokenAddr.length; i++){
-            ERC20Contract = ERC20(_tokenAddr[i]);
-            ERC20Contract.transferFrom(msg.sender, vault, _amount[i] * (10 ** decimals(_tokenAddr[i])));
-        
-            emit TokenReceived(ERC20Contract.name(), ERC20Contract.decimals(), _amount[i], msg.sender);
-        }
+ function receiveTokens(address _tokenAddr, uint _amount) external {
+        require(whitelist[_tokenAddr], "Token not accepted");
+        require(_amount > 0, "Amount Not Valid");
+        ERC20Contract = ERC20(_tokenAddr);
+        ERC20Contract.transferFrom(msg.sender, vault, _amount);
+
+        emit TokenReceived(ERC20Contract.name(), ERC20Contract.decimals(), _amount, msg.sender);
     }
     
     function checkWhitelisted(address [] memory tokens) private view returns(bool){
@@ -37,27 +43,28 @@ contract TokenReceiver is Ownable{
     }
     
     function addToWhitelist(address _tokenAddr) external onlyOwner {
-        require(_tokenAddr != address(0), "Address not eligible");
+        require(_tokenAddr != address(0), "addToWhitelist: 0 Address cannot be added");
         whitelist[_tokenAddr] = true;
     }
     
     function removeFromWhitelist(address _tokenAddr) external onlyOwner {
-        require(_tokenAddr != address(0), "Address not eligible");
+        require(_tokenAddr != address(0), "removeFromWhitelist: Wrong Address");
         whitelist[_tokenAddr] = false;
     }
     
     function changeWalletAddress(address payable _newWallet) external onlyOwner {
         vault = _newWallet;
+        emit vaultChanged(_newWallet);
     }
     
-    function decimals(address _tokenAddr) internal returns(uint) {
+    function decimals(address _tokenAddr) public returns(uint) {
         ERC20Contract = ERC20(_tokenAddr);
         return ERC20Contract.decimals();
     }
     
     receive() external payable {
         vault.transfer(msg.value);
-        emit Received(msg.sender, msg.value);
+        emit EtherReceived(msg.sender, msg.value);
     }
     
 }
