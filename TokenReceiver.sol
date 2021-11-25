bffd315fc78289f8267cc6172bf7ec0cccc12090
Zain Raza <76092682+OptimusZain@users.noreply.github.com>
2021-11-25 13:05:08 +0500

    Hardcoding certain Addresses

--- TokenReceiver.sol
+++ TokenReceiver.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TokenReceiver is Ownable{
+contract payment is Ownable{
     
     event EtherReceived(address from, uint value);
     event vaultChanged(address new_address);
@@ -15,14 +15,36 @@ contract TokenReceiver is Ownable{
         address Address;
     }
 
+    struct test{
+        uint num;
+    }
+
     ERC20 ERC20Contract;
     mapping(address => bool) public whitelist;
-    Tokens [] public TokensList;
+    Tokens [] private TokensList;
 
     address payable vault;
-    
+    test [] public Value;
+ 
     constructor() {
-        vault = payable(owner());
+
+    vault = payable(owner());
+   
+    // addresses to receive payments
+        address[4] memory tokens = [
+        0xaD6D458402F60fD3Bd25163575031ACDce07538D,
+        0x6EE856Ae55B6E1A249f04cd3b947141bc146273c,
+        0xE1E545c89F3996Cf06E1f12C6D05e3412c7C7b11,
+        0x5D9Bb02f1ac65d10e16EED5E2212D31E6AdD0b04
+        // 0x512a34a032116ecde07bfe47e731b2d16b77a5fb,
+        // 0x512a34a032116ecde07bfe47e731b2d16b77a5fb,
+        // 0x512a34a032116ecde07bfe47e731b2d16b77a5fb,
+        // 0x0F5D2fB29fb7d3CFeE444a200298f468908cC942
+        ];
+
+        for(uint i = 0; i < tokens.length; i++){
+            addToWhitelist(tokens[i]);
+        }
     } 
     
     function receiveTokens(address _tokenAddr, uint _amount) external {
@@ -34,15 +56,15 @@ contract TokenReceiver is Ownable{
         emit TokenReceived(ERC20Contract.name(), ERC20Contract.decimals(), _amount, msg.sender);
     }
     
-    function checkWhitelisted(address [] memory tokens) private view returns(bool){
-        for(uint i = 0; i < tokens.length; i++){
-            if(whitelist[tokens[i]] == false)
+    function checkWhitelisted(address [] memory token) private view returns(bool){
+        for(uint i = 0; i < token.length; i++){
+            if(whitelist[token[i]] == false)
                 return false;
         }
         return true;
     }
     
-    function addToWhitelist(address _tokenAddr) external onlyOwner {
+    function addToWhitelist(address _tokenAddr) public onlyOwner {
         require(_tokenAddr != address(0), "addToWhitelist: 0 Address cannot be added");
         require(whitelist[_tokenAddr] != true, "addToWhitelist: Already Whitelisted");
 
@@ -78,7 +100,7 @@ contract TokenReceiver is Ownable{
         return TokensList;
     } 
     
-    function decimals(address _tokenAddr) public returns(uint) {
+    function getContractDecimals(address _tokenAddr) public returns(uint) {
         ERC20Contract = ERC20(_tokenAddr);
         return ERC20Contract.decimals();
     }
